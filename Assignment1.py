83aa644e44efa8921402f71ea31e0f52d9e40587
sharvi2311 <chougulesharvi@gmail.com>
2026-08-20 22:24:38 +0530

    Largest number of the 3 user inputs

--- Assignment1.py
+++ Assignment1.py
@@ -1,25 +1,27 @@
-a = int(input("Enter number A = "))
-b = int(input("Enter number B = "))
-c = int(input("Enter number C = "))
+A = int(input("Enter number A = "))
+B = int(input("Enter number B = "))
+C = int(input("Enter number C = "))
 
-if (a==b==c):
+if (A==B==C):
     print("A,B,C are equal")
-elif(a==b or b==c or c==a):
-    if(a==b and a>c):
+
+elif(A==B or B==C or C==A):
+    if(A==B and A>C):
         print("A and B are equal and greater than C")
-    elif(b==c and b>a):
+    elif(B==C and B>A):
         print("B and C are equal and greater than A")
-    elif(c==a and a>b):
+    elif(C==A and A>B):
         print("A and C are equal and greater than B")
-    elif(a==b and a<c):
+    elif(A==B and A<C):
         print("A and B are equal and greater than C")
-    elif(b==c and b<a):
-        print("B and C are equal and greater than C")
-    elif(a==c and a<b):
+    elif(A==C and A<B):
         print("A and C are equal and greater than C")
-elif (a>b and a>c):
+    elif(B==C and B<A):
+        print("B and C are equal and greater than C")
+
+elif (A>B and A>C):
     print("A is largest among three")
-elif (b>a and b>c):
+elif (B>A and B>C):
     print("B is largest among three")
 else:
     print("C is largest among three")
